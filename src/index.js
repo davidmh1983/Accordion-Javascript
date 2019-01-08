0d267495b22176750898b7bffa6950dff487d43c
davidmh1983 <35410567+davidmh1983@users.noreply.github.com>
2019-01-08 13:35:06 +0100

    Improvement as second level didn't work
    Changes for second level

--- src/index.js
+++ src/index.js
@@ -1,48 +1,39 @@
 import "./styles.scss";
 
-document.getElementById("app").innerHTML = `
-<dl>
-  <dt>Section 1</dt>
-  <dd>
-    <p>Section 1 Content...</p>
-  </dd>
-  <dt>Section 2</dt>
-  <dd>
-    <p>Section 2 Content...</p>
-  </dd>
-  <dt>Section 3</dt>
-  <dd>
-    <p>Section 3 Content...</p>
-  </dd>
-</dl>
-`;
-
 var accordion = document.getElementsByTagName("dl");
 accordionDisplay();
 
 function accordionDisplay() {
+  //Attach the event to all the buttons
   for (var j = 0; j < accordion.length; j++) {
     for (var i = 0; i < accordion[j].children.length; i = i + 2) {
       accordion[j].children[i].addEventListener("click", function() {
         var dt = this.nextElementSibling;
+        //When click, put all in inactive
         for (var k = 0; k < accordion.length; k++) {
           for (var l = 0; l < accordion[k].children.length; l = l + 2) {
+            //Only the ones from that same list that are active
             if (
               accordion[k].children[l].className === "active" &&
-              this !== accordion[k].children[l]
+              this !== accordion[k].children[l] &&
+              this.parentElement === accordion[k]
             ) {
-              accordion[k].children[l].className = "";
+              accordion[k].children[l].classList.toggle("active");
               accordion[k].children[l + 1].style.maxHeight = null;
               break;
             }
           }
         }
+        //Swith the one clicked to active
         this.classList.toggle("active");
         if (dt.style.maxHeight) {
           dt.style.maxHeight = null;
         } else {
-          dt.style.maxHeight = dt.scrollHeight + "px";
-          //dt.style.maxHeight = "100%";
+          if (accordion.length === 1) {
+            dt.style.maxHeight = dt.scrollHeight + "px";
+          } else {
+            dt.style.maxHeight = "100%";
+          }
         }
       });
     }
